40cf3ec6e900092abaaf7ffdf7cf8caa559ded7e
JSanry <82422370+JSanry@users.noreply.github.com>
2023-10-24 21:33:01 -0300

    Update 2_Picket_Fence.py

--- pages/2_Picket_Fence.py
+++ pages/2_Picket_Fence.py
@@ -7,12 +7,14 @@ from datetime import date
 from fpdf import FPDF
 import matplotlib.pyplot as plt
 
+
+mod = "https://raw.githubusercontent.com/JSanry/teste-pylinac/main/picketfence.txt"
 #mod = "/mount/src/teste-pylinac/picketfence.txt"
-#import pylinac.picketfence
-#with open(mod,'r') as writer_file:
-#    contents_to_write = writer_file.read()
-#with open(pylinac.picketfence.__file__,'w') as file_to_overwrite:
-#    file_to_overwrite.write(contents_to_write)
+import pylinac.picketfence
+with open(mod,'r') as writer_file:
+    contents_to_write = writer_file.read()
+with open(pylinac.picketfence.__file__,'w') as file_to_overwrite:
+    file_to_overwrite.write(contents_to_write)
 from pylinac.picketfence import PicketFence, MLCArrangement, MLC
 
 import streamlit as st
