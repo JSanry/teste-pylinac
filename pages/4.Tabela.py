12f7ecba8555298e9a0f3234a297eea79431154a
JSanry <82422370+JSanry@users.noreply.github.com>
2023-10-25 11:18:18 +0000

    y

---
+++ pages/4.Tabela.py
@@ -0,0 +1,36 @@
+from urllib.error import URLError
+
+import altair as alt
+import pandas as pd
+from PIL import Image
+from datetime import date
+from fpdf import FPDF
+
+from pylinac import Starshot
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+
+
+def Tabela():
+
+    # Create a connection object.
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+    df = conn.read()
+
+    # Print results.
+    for row in df.itertuples():
+        st.write(f"{row.name} has a :{row.pet}:")
+    
+st.set_page_config(page_title="registro", page_icon="🎇")
+
+
+
+st.sidebar.header("Tabela")
+
+Tabela()
+
+show_code(Tabela)
