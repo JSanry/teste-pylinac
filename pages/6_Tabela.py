47198547d645b1b18a48e418289f2f9b9b08d2f2
JSanry <82422370+JSanry@users.noreply.github.com>
2024-01-22 13:16:09 +0000

    new

--- pages/6_Tabela.py
+++ pages/6_Tabela.py
@@ -6,17 +6,17 @@ import pandas as pd
 
 def Tabela():
     # Display Title and Description
-    st.title("Vendor Management Portal")
+    st.title("Registro de Testes")
 
     # Constants
-    BusinessType = [
+    FISICOS = [
         "Manufacturer",
         "Distributor",
         "Wholesaler",
         "Retailer",
         "Service Provider",
     ]
-    PRODUCTS = [
+    UNIDADE = [
         "Electronics",
         "Apparel",
         "Groceries",
@@ -34,21 +34,20 @@ def Tabela():
     action = st.selectbox(
         "Choose an Action",
         [
-            "Onboard New Vendor",
-            "Update Existing Vendor",
-            "View All Vendors",
-            "Delete Vendor",
+            "Dados novo teste",
+            "Ver todos dados",
+            "Deletar dado",
         ],
     )
 
-    if action == "Onboard New Vendor":
+    if action == "Dados novo teste":
         st.markdown("Enter the details of the new vendor below.")
-        with st.form(key="vendor_form"):
+        with st.form(key="teste_form"):
             company_name = st.text_input(label="Company Name*")
             business_type = st.selectbox(
-                "Business Type*", options=BUSINESS_TYPES, index=None
+                "Business Type*", options=FISICOS, index=None
             )
-            products = st.multiselect("Products Offered", options=PRODUCTS)
+            products = st.multiselect("Products Offered", options=UNIDADE)
             onboarding_date = st.date_input(label="Onboarding Date")
 
             st.markdown("**required*")
@@ -57,10 +56,10 @@ def Tabela():
             if submit_button:
                 if not company_name or not business_type:
                     st.warning("Ensure all mandatory fields are filled.")
-                elif existing_data["CompanyName"].str.contains(company_name).any():
+                elif existing_data["Teste"].str.contains(company_name).any():
                     st.warning("A vendor with this company name already exists.")
                 else:
-                    vendor_data = pd.DataFrame(
+                    teste_data = pd.DataFrame(
                         [
                             {
                                 "Teste": company_name,
@@ -70,99 +69,31 @@ def Tabela():
                             }
                         ]
                     )
-                    updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
-                    conn.update(worksheet="Vendors", data=updated_df)
-                    st.success("Vendor details successfully submitted!")
-
-    elif action == "Update Existing Vendor":
-        st.markdown("Select a vendor and update their details.")
-
-        vendor_to_update = st.selectbox(
-            "Select a Vendor to Update", options=existing_data["CompanyName"].tolist()
-        )
-        vendor_data = existing_data[existing_data["CompanyName"] == vendor_to_update].iloc[
-            0
-        ]
-
-        with st.form(key="update_form"):
-            company_name = st.text_input(
-                label="Company Name*", value=vendor_data["CompanyName"]
-            )
-            business_type = st.selectbox(
-                "Business Type*",
-                options=BUSINESS_TYPES,
-                index=BUSINESS_TYPES.index(vendor_data["BusinessType"]),
-            )
-            products = st.multiselect(
-                "Products Offered",
-                options=PRODUCTS,
-                default=vendor_data["Products"].split(", "),
-            )
-            years_in_business = st.slider(
-                "Years in Business", 0, 50, int(vendor_data["YearsInBusiness"])
-            )
-            onboarding_date = st.date_input(
-                label="Onboarding Date", value=pd.to_datetime(vendor_data["OnboardingDate"])
-            )
-            additional_info = st.text_area(
-                label="Additional Notes", value=vendor_data["AdditionalInfo"]
-            )
-
-            st.markdown("**required*")
-            update_button = st.form_submit_button(label="Update Vendor Details")
-
-            if update_button:
-                if not company_name or not business_type:
-                    st.warning("Ensure all mandatory fields are filled.")
-                else:
-                    # Removing old entry
-                    existing_data.drop(
-                        existing_data[
-                            existing_data["CompanyName"] == vendor_to_update
-                        ].index,
-                        inplace=True,
-                    )
-                    # Creating updated data entry
-                    updated_vendor_data = pd.DataFrame(
-                        [
-                            {
-                                "CompanyName": company_name,
-                                "BusinessType": business_type,
-                                "Products": ", ".join(products),
-                                "YearsInBusiness": years_in_business,
-                                "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-                                "AdditionalInfo": additional_info,
-                            }
-                        ]
-                    )
-                    # Adding updated data to the dataframe
-                    updated_df = pd.concat(
-                        [existing_data, updated_vendor_data], ignore_index=True
-                    )
-                    conn.update(worksheet="Vendors", data=updated_df)
-                    st.success("Vendor details successfully updated!")
+                    updated_df = pd.concat([existing_data, teste_data], ignore_index=True)
+                    conn.update(worksheet="Teste", data=updated_df)
+                    st.success("Registro feito!")
 
     # View All Vendors
-    elif action == "View All Vendors":
+    elif action == "Ver todos dados":
         st.dataframe(existing_data)
 
     # Delete Vendor
-    elif action == "Delete Vendor":
-        vendor_to_delete = st.selectbox(
-            "Select a Vendor to Delete", options=existing_data["CompanyName"].tolist()
+    elif action == "Deletar dado":
+        test_to_delete = st.selectbox(
+            "Selecionar teste para deletar", options=existing_data["Teste"].tolist()
         )
 
         if st.button("Delete"):
             existing_data.drop(
-                existing_data[existing_data["CompanyName"] == vendor_to_delete].index,
+                existing_data[existing_data["Teste"] == test_to_delete].index,
                 inplace=True,
             )
-            conn.update(worksheet="Vendors", data=existing_data)
-            st.success("Vendor successfully deleted!")
+            conn.update(worksheet="Teste", data=existing_data)
+            st.success("Teste deletado!")
 
 st.sidebar.header("Tabela")
 #st.write("""Teste""")
 
 Tabela()
 
-show_code(Tabela)
+#show_code(Tabela)
