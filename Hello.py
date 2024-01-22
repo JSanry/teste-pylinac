1b5fed1d544d286d3537aad191624ebf9809131c
JSanry <82422370+JSanry@users.noreply.github.com>
2024-01-22 13:59:07 +0000

    c

--- Hello.py
+++ Hello.py
@@ -16,6 +16,8 @@
 import streamlit as st
 from streamlit.logger import get_logger
 from PIL import Image
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
 LOGGER = get_logger(__name__)
 
@@ -23,31 +25,119 @@ LOGGER = get_logger(__name__)
 def run():
     st.set_page_config(
         page_title="TESTES Pylinac",
-        page_icon="👋",
+        page_icon="📋",
     )
     #image = Image.open('logoinrad.png')
 
     #st.image(image, caption='Sunrise by the mountains')
-    st.write("# Welcome to Streamlit! 💾")
+    st.write("# Registro dos Testes📋")
     st.sidebar.success("Selecione Testes Acima")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Dados registrados dos testes
     """
     )
+    # Titulo
+    st.title("Registro de Testes")
+
+    # Constantes
+    FISICOS = [
+        "Manufacturer",
+        "Distributor",
+        "Wholesaler",
+        "Retailer",
+        "Service Provider",
+    ]
+    UNIDADE = [
+        "Electronics",
+        "Apparel",
+        "Groceries",
+        "Software",
+        "Other",
+    ]
+
+    teste_dados = st.selectbox(
+        "Escolha o teste",
+        [
+            "Teste",
+            "Star",
+        ],
+    )
+
+    # Establishing a Google Sheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # Fetch existing vendors data
+    existing_data = conn.read(worksheet=teste_dados, usecols=list(range(4)), ttl=5)
+    existing_data = existing_data.dropna(how="all")
+
+    action = st.selectbox(
+        "Escolha uma ação",
+        [
+        
+            "Ver todos dados",
+            "Dados novo teste",
+            "Deletar dado",
+        ],
+    )
+
+    if action == "Dados novo teste":
+        st.markdown("Enter the details of the new vendor below.")
+        with st.form(key="teste_form"):
+            company_name = st.text_input(label="Company Name*")
+            business_type = st.selectbox(
+                "Business Type*", options=FISICOS, index=None
+            )
+            products = st.multiselect("Products Offered", options=UNIDADE)
+            onboarding_date = st.date_input(label="Onboarding Date")
+
+            st.markdown("**required*")
+            submit_button = st.form_submit_button(label="Submit Vendor Details")
+
+            if submit_button:
+                if not company_name or not business_type:
+                    st.warning("Ensure all mandatory fields are filled.")
+                elif existing_data["Teste"].str.contains(company_name).any():
+                    st.warning("A vendor with this company name already exists.")
+                else:
+                    teste_data = pd.DataFrame(
+                        [
+                            {
+                                "Teste": company_name,
+                                "Fisico": business_type,
+                                "Aparelho": ", ".join(products),
+                                "Dia": onboarding_date.strftime("%Y-%m-%d"),
+                            }
+                        ]
+                    )
+                    updated_df = pd.concat([existing_data, teste_data], ignore_index=True)
+                    conn.update(worksheet=teste_dados, data=updated_df)
+                    st.success("Registro feito!")
+
+    # View All Vendors
+    elif action == "Ver todos dados":
+        st.dataframe(existing_data)
+
+    # Delete Vendor
+    elif action == "Deletar dado":
+        test_to_delete = st.selectbox(
+            "Selecionar teste para deletar", options=existing_data["Teste"].tolist()
+        )
+
+        if st.button("Delete"):
+            existing_data.drop(
+                existing_data[existing_data["Teste"] == test_to_delete].index,
+                inplace=True,
+            )
+            conn.update(worksheet=teste_dados, data=existing_data)
+            st.success("Teste deletado!")
+
+
+
+
+
+
 if __name__ == "__main__":
     run()
 
