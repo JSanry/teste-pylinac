45a9410f87b189a28c305fe640a68cae6cc455de
JSanry <82422370+JSanry@users.noreply.github.com>
2024-11-17 13:35:31 +0000

    j

--- pages/starshot.py
+++ pages/starshot.py
@@ -40,18 +40,78 @@ def show_SS():
     dpi_inp = st.sidebar.number_input(label='DPI',step=1.0 ,format="%.1f",min_value=80.0, max_value=200.0, value=100.0)
     sid_inp = st.sidebar.number_input(label='SID (mm)',step=1.0 ,format="%.1f",min_value=700.0, max_value=1900.0, value=1000.0)
     r_input = st.sidebar.checkbox (label="Recursivo", value= True)
+    multiple = st.sidebar.checkbox (label="Multiplas imagens", value= False)
 
     Logo =st.sidebar.checkbox( label= 'Logo no PDF', value= True)
 
     #upload imagem
     #analise da imagem
-    star_img = st.file_uploader('upload', accept_multiple_files=True, label_visibility= "hidden")
-    #star_img = st.file_uploader(label="upload", label_visibility= "hidden")
-    if star_img is not None and len(star_img)==1:
+
+    if multiple:
+        star_img = st.file_uploader('upload', accept_multiple_files=True, label_visibility= "hidden")
+    else:
+        star_img = st.file_uploader(label="upload", label_visibility= "hidden")
+
+    if star_img is not None and not(multiple):
         my_star = Starshot(star_img, dpi=dpi_inp, sid=sid_inp)
         my_star.analyze(radius=r, tolerance=tol, recursive=r_input)
         data = my_star.results_data()
-    elif star_img is not None and len(star_img)>=2:
+
+        if data.passed:
+            st.markdown("### Resultado Passou ")
+        else:
+            st.markdown("### Resultado Não Passou! ")
+
+        #Resultados  
+    
+        st.write("Círculo mínimo tem o diâmetro de" , "%.3f" %data.circle_diameter_mm, "mm")
+        st.write("O centro do círculo ocorre em" , "%.1f" %data.circle_center_x_y[0], ",","%.1f" %data.circle_center_x_y[1])
+        
+        #Mostra imagens
+        my_star.save_analyzed_image("mystar.png")
+        img_star= Image.open('mystar.png')
+        st.image(img_star, output_format="auto")
+        
+        #Definições para PDF e Registro
+        st.title('Defenições PDF')
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            Unit = st.selectbox('Unidade',('iX', '6EX', 'True Beam',"Outra opção..."), index= None)
+            if Unit == "Outra opção...":
+                Unit = st.text_input("Digite a Unidade...")
+
+        with col2:
+            Fis = st.selectbox('Físico',('Laura', 'Victor', 'Marcus', "Outra opção..."),index= None)
+            if Fis == "Outra opção...":
+                Fis = st.text_input("Digite o operador...")
+
+        with col3:
+            Par = st.selectbox('Parâmetro',('Gantry','Mesa', 'Col' ),index= None)
+
+        dia = st.date_input("Data de realização do teste:", value= date.today())    
+        data_teste = dia.strftime("%m-%d-%Y")
+
+        if not Unit or not Par or not Fis:
+            st.warning("Preencher campos de registro faltantes")
+        else:
+            nomepdf = 'StarShot_' + Unit + '_' + Par + '_' + data_teste +'.pdf'
+       
+        #Gerar pdf
+            if Logo:      
+                my_star.publish_pdf(filename="res.pdf",open_file=False, logo="https://raw.githubusercontent.com/JSanry/teste-pylinac/main/logoinrad.png" , metadata={'Físico': Fis, 'Unidade': Unit, 'Parâmetro': Par, 'Data': data_teste, 'Raio Analise':r})
+            else:
+                my_star.publish_pdf(filename="res.pdf",open_file=False, metadata={'Físico': Fis, 'Unidade': Unit, 'Parâmetro': Par, 'Data': data_teste, 'Raio Analise':r})
+            with open("res.pdf", "rb") as pdf_file:
+                PDFbyte = pdf_file.read()
+            st.success("PDF gerado!")
+            st.download_button(label="Download PDF",
+                               data=PDFbyte,
+                               file_name=nomepdf,
+                               mime='application/octet-stream')   
+        
+    
+    elif star_img is not None and multiple and len(star_img)>=2:
         my_star = Starshot.from_multiple_images(star_img,dpi=dpi_inp, sid=sid_inp)
         my_star.analyze(radius=r, tolerance=tol, recursive=r_input)
         data = my_star.results_data()
