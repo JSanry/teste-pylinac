4c6027f97a68ed7e98d3adb13e7cd06fc295684c
JSanry <82422370+JSanry@users.noreply.github.com>
2026-03-17 19:09:03 +0000

    p1

--- pages/Picket_Fence.py
+++ pages/Picket_Fence.py
@@ -112,7 +112,7 @@ def show_PF():
        
         #Gerar pdf
             
-            pf.publish_pdf(filename="res.pdf",open_file=False, logo="https://raw.githubusercontent.com/JSanry/teste-pylinac/USER-EXT/logoDOR.png" , metadata={'Físico': Fis, 'Unidade': Unit, 'Data': data_teste})
+            pf.publish_pdf(filename="res.pdf",open_file=False, logo="https://raw.githubusercontent.com/JSanry/teste-pylinac/USER-EXT/logoP.png" , metadata={'Físico': Fis, 'Unidade': Unit, 'Data': data_teste})
             with open("res.pdf", "rb") as pdf_file:
                 PDFbyte = pdf_file.read()
             st.sidebar.success("PDF gerado!")
